176a5c1f40b576e1645615700817b731396425cd
Michael Holt <mike@holt.org>
2014-06-20 18:36:21 -0700

    Vendor library updates

--- test/vendor/sinon-ie-1.10.2.js
+++ test/vendor/sinon-ie-1.10.2.js
@@ -1,12 +1,12 @@
 /**
- * Sinon.JS 1.7.3, 2013/06/20
+ * Sinon.JS 1.10.2, 2014/06/02
  *
  * @author Christian Johansen (christian@cjohansen.no)
  * @author Contributors: https://github.com/cjohansen/Sinon.JS/blob/master/AUTHORS
  *
  * (The BSD License)
  * 
- * Copyright (c) 2010-2013, Christian Johansen, christian@cjohansen.no
+ * Copyright (c) 2010-2014, Christian Johansen, christian@cjohansen.no
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification,
@@ -49,6 +49,8 @@
  */
 function setTimeout() {}
 function clearTimeout() {}
+function setImmediate() {}
+function clearImmediate() {}
 function setInterval() {}
 function clearInterval() {}
 function Date() {}
@@ -57,6 +59,8 @@ function Date() {}
 // should be true. Hackish, I know, but it works.
 setTimeout = sinon.timers.setTimeout;
 clearTimeout = sinon.timers.clearTimeout;
+setImmediate = sinon.timers.setImmediate;
+clearImmediate = sinon.timers.clearImmediate;
 setInterval = sinon.timers.setInterval;
 clearInterval = sinon.timers.clearInterval;
 Date = sinon.timers.Date;
@@ -80,3 +84,17 @@ function XMLHttpRequest() {}
 // Reassign the original function. Now its writable attribute
 // should be true. Hackish, I know, but it works.
 XMLHttpRequest = sinon.xhr.XMLHttpRequest || undefined;
+/*global sinon*/
+/**
+ * Helps IE run the fake XDomainRequest. By defining global functions, IE allows
+ * them to be overwritten at a later point. If these are not defined like
+ * this, overwriting them will result in anything from an exception to browser
+ * crash.
+ *
+ * If you don't require fake XDR to work in IE, don't include this file.
+ */
+function XDomainRequest() {}
+
+// Reassign the original function. Now its writable attribute
+// should be true. Hackish, I know, but it works.
+XDomainRequest = sinon.xdr.XDomainRequest || undefined;
